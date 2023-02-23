d68bf454b5d567f6fc7766fae245d68fabcbb2a1
Kaz1deX <max2003255@gmail.com>
2023-02-23 20:15:12 +0300

    Added ViewBinding

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.androidapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyButton";
     @Override
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView2 = (TextView) findViewById(R.id.textView2);
-        textView2.setText("Введите свое имя");
+        textView2.setText(R.string.enter_name);
 
         TextView editTextTextPersonName2 = (TextView) findViewById(R.id.editTextTextPersonName2);
         editTextTextPersonName2.setHint("Имя");
@@ -35,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         button2.setOnClickListener(listener);
+
+        // Использование ViewBinding
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textView2.setText(R.string.enter_name);
+        binding.editTextTextPersonName2.setHint("Имя");
+        binding.button2.setText("Далее");
+        binding.imageView2.setImageResource(R.drawable.greenhowl);
+
+        binding.button2.setOnClickListener(listener);
     }
 
     public void clickMyButton(View view){
