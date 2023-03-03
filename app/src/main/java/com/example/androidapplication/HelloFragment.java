903c982df97f160c2c534b0abaa7ad60ef60caee
Kaz1deX <max2003255@gmail.com>
2023-03-03 03:43:50 +0300

    Transferring layout from activity to fragment

--- app/src/main/java/com/example/androidapplication/HelloFragment.java
+++ app/src/main/java/com/example/androidapplication/HelloFragment.java
@@ -1,9 +1,30 @@
 package com.example.androidapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class HelloFragment extends Fragment {
+    FragmentManager fragmentManager;
     public HelloFragment(){
         super(R.layout.fragment_hello);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button_hello_fragment = (Button) view.findViewById(R.id.button_hello_fragment);
+        button_hello_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -1,20 +1,47 @@
 package com.example.androidapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null){
-            fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().setReorderingAllowed(true).add(R.id.fragment_container_view, HelloFragment.class, null).commit();
-        }
+//        if (savedInstanceState == null){
+//            getSupportFragmentManager().beginTransaction()
+//                    .setReorderingAllowed(true)
+//                    .add(R.id.fragment_container_view,
+//                            HelloFragment.class, null).commit();
+//        }
+
+        ImageButton name_settings = (ImageButton) findViewById(R.id.name_settings);
+        name_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .add(R.id.fragment_container_view,
+                                HelloFragment.class, null).commit();
+            }
+        });
+
+        ImageButton name_settings2 = (ImageButton) findViewById(R.id.name_settings2);
+        name_settings2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .add(R.id.fragment_container_view,
+                                ConfirmFragment.class, null).commit();
+            }
+        });
     }
 }
