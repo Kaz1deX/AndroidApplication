9b6b5bbf629352c36ca316e7e5490a112743c75d
Kaz1deX <max2003255@gmail.com>
2023-03-10 18:52:17 +0300

    Added 2 buttons that go to the second and third fragments

--- app/src/main/java/com/example/androidapplication/HelloFragment.java
+++ app/src/main/java/com/example/androidapplication/HelloFragment.java
@@ -1,16 +1,13 @@
 package com.example.androidapplication;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentResultListener;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.androidapplication.databinding.FragmentHelloBinding;
 
@@ -39,19 +36,21 @@ public class HelloFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         fragmentManager = getParentFragmentManager();
-        fragmentManager.setFragmentResultListener("requestKey", this, new FragmentResultListener() {
-            @Override
-            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle bundle) {
-                String result = bundle.getString("bundleKey");
-                binding.enterName.setText(result + "?");
-            }
-        });
 
         binding.buttonHelloFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.fragment_container_view, new ListViewFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        binding.buttonHelloFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, new RecyclerViewFragment());
                 fragmentTransaction.commit();
             }
         });
