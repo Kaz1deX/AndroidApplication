1a278c9402e7d7e11c1157b33af4f5ea802d2ca3
Kaz1deX <max2003255@gmail.com>
2023-04-07 21:31:26 +0300

    Created the model package

---
+++ app/src/main/java/com/example/androidapplication/model/Question.java
@@ -0,0 +1,23 @@
+package com.example.androidapplication.model;
+
+public class Question {
+    private String question;
+    private int number;
+
+    public Question() {
+        this("No question", 0);
+    }
+
+    public Question(String question, int number) {
+        this.question = question;
+        this.number = number;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
---
+++ app/src/main/java/com/example/androidapplication/model/Topic.java
@@ -0,0 +1,23 @@
+package com.example.androidapplication.model;
+
+public class Topic {
+    private String topic;
+    private int number;
+
+    public Topic() {
+        this("No topic", 0);
+    }
+
+    public Topic(String topic, int number) {
+        this.topic = topic;
+        this.number = number;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
