7d61984a8129d44b0ccb8c05ef9cfd9a5d64a9d0
Kaz1deX <max2003255@gmail.com>
2023-04-21 22:41:32 +0300

    Added app-specific storage

--- app/src/main/java/com/example/androidapplication/ui/fragments/NameFragment.java
+++ app/src/main/java/com/example/androidapplication/ui/fragments/NameFragment.java
@@ -1,5 +1,6 @@
 package com.example.androidapplication.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,13 +8,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.androidapplication.R;
 import com.example.androidapplication.databinding.FragmentHelloBinding;
 import com.example.androidapplication.databinding.FragmentNameBinding;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class NameFragment extends Fragment {
     FragmentNameBinding binding;
@@ -43,6 +51,13 @@ public class NameFragment extends Fragment {
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
                 bundle.putString("name", binding.editTextTextPersonName4.getText().toString());
+
+                String student_name = binding.editTextTextPersonName4.getText().toString();
+                String file_name = "studentName.txt";
+
+                // app-specific storage
+                createFileAppSpecificStorage(file_name, student_name);
+
                 Navigation.findNavController(view).navigate(R.id.action_nameFragment_to_statisticFragment, bundle);
             }
         });
@@ -53,5 +68,18 @@ public class NameFragment extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_nameFragment_to_recyclerViewFragment2);
             }
         });
+    }
+
+    void createFileAppSpecificStorage(String file_name, String student_name){
+        Context context = getContext();
+        File directory = context.getFilesDir();
+        File file = new File(directory, file_name);
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(student_name);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
